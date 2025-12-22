e440eeb8f6295d9fb224fd80a8a1c046a0dc5cf3
Alex Moronto <toruk010994@gmail.com>
2025-12-22 16:37:19 +0400

    Enhance Swagger configuration with custom API details and UI endpoint

--- Nimbo.Wms/Program.cs
+++ Nimbo.Wms/Program.cs
@@ -1,15 +1,31 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Nimbo.Wms",
+            Version = "v1"
+        });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nimbo WMS API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
