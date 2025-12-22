1697e9609b75f8ddae5b12a9909fa65018229aca
Alex Moronto <toruk010994@gmail.com>
2025-12-22 19:01:51 +0400

    Add `ZoneType` and `LocationType` enums to `References` namespace

---
+++ Nimbo.Wms/Domain/References/ZoneType.cs
@@ -0,0 +1,13 @@
+﻿namespace Nimbo.Wms.Domain.References;
+
+public enum ZoneType
+{
+    Storage = 1,
+    Picking,
+    Receiving,
+    Shipping,
+    Staging,
+    Quarantine,
+    Damaged,
+    Returns,
+}
