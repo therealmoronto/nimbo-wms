f0ed38d4634cb13dde8039d3766788f1b2f9b819
Alex Moronto <toruk010994@gmail.com>
2025-12-25 17:47:40 +0400

    Add `InventoryCount`, `InventoryCountLine`, `InventoryCountId`, and `InventoryCountStatus` entities to domain models with validation and state management logic

---
+++ Nimbo.Wms/Domain/Identification/InventoryCountId.cs
@@ -0,0 +1,15 @@
+﻿namespace Nimbo.Wms.Domain.Identification;
+
+public readonly struct InventoryCountId : IEntityId
+{
+    public InventoryCountId(Guid value)
+    {
+        EntityId.EnsureNotEmpty<InventoryCountId>(value);
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static InventoryCountId New() => EntityId.New(id => new InventoryCountId(id));
+    public static InventoryCountId From(Guid guid) => EntityId.From(guid, id => new InventoryCountId(id));
+}
