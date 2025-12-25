a9eb61cab0510efac6821a84cdbfce19f601dd25
Alex Moronto <toruk010994@gmail.com>
2025-12-25 18:41:05 +0400

    Add `ShipmentOrder`, `ShipmentOrderLine`, `ShipmentOrderId`, `ShipmentOrderStatus`, `Customer`, and `CustomerId` entities to domain models with validation and state management logic

---
+++ Nimbo.Wms/Domain/Identification/ShipmentOrderId.cs
@@ -0,0 +1,22 @@
+﻿namespace Nimbo.Wms.Domain.Identification;
+
+public readonly struct ShipmentOrderId : IEntityId
+{
+    public ShipmentOrderId(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new ArgumentException("ShipmentOrderId cannot be empty.", nameof(value));
+
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static ShipmentOrderId New() => EntityId.New(id => new ShipmentOrderId(id));
+
+    public static ShipmentOrderId From(Guid guid) => EntityId.From(guid, id => new ShipmentOrderId(id));
+
+    public override string ToString() => Value.ToString("D");
+
+    public static implicit operator Guid(ShipmentOrderId id) => id.Value;
+}
