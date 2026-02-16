98fa0e6f1fe4471475c0da4d6f66b729432f9391
Alexander Morgunov <toruk010994@gmail.com>
2026-02-16 20:06:04 +0400

    Enforce `Barcode` as a required field for items in the database schema. Add migration to update the column definition and ensure consistency with domain rules.

---
+++ Nimbo.Wms.Infrastructure/Migrations/20260216155709_Item_BarcodeIsRequiredField.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nimbo.Wms.Migrations
+{
+    /// <inheritdoc />
+    public partial class Item_BarcodeIsRequiredField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                schema: "nimbo",
+                table: "items",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                schema: "nimbo",
+                table: "items",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+        }
+    }
+}
