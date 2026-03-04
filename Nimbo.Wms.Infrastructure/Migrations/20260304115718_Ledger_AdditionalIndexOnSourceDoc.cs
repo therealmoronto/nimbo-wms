6178f4a6e9811f70bb30d3a2396cd317273dd3c6
Alexander Morgunov <toruk010994@gmail.com>
2026-03-04 15:57:41 +0400

    nimbo-wms-120: Add migration for additional index on `SourceDocumentId` in `StockLedgerEntry` table

---
+++ Nimbo.Wms.Infrastructure/Migrations/20260304115718_Ledger_AdditionalIndexOnSourceDoc.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nimbo.Wms.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ledger_AdditionalIndexOnSourceDoc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_stock_ledger_entries_SourceDocumentId",
+                schema: "nimbo",
+                table: "stock_ledger_entries",
+                column: "SourceDocumentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_stock_ledger_entries_SourceDocumentId",
+                schema: "nimbo",
+                table: "stock_ledger_entries");
+        }
+    }
+}
