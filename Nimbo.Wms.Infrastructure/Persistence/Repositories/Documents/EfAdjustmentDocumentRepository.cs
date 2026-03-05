060f5c613c9d32eb19c5577c7ec338fe1a027b32
Alexander Morgunov <toruk010994@gmail.com>
2026-03-05 17:38:32 +0400

    nimbo-wms-127: Add repository and posting service for `AdjustmentDocument`
    - Introduced `IAdjustmentDocumentRepository` interface with EF implementation.
    - Created `AdjustmentDocumentPostingService` to manage posting logic.
    - Implemented entity retrieval, validation, and stock adjustments in the posting process.

---
+++ Nimbo.Wms.Infrastructure/Persistence/Repositories/Documents/EfAdjustmentDocumentRepository.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Nimbo.Wms.Application.Abstractions.Persistence.Repositories.Documents;
+using Nimbo.Wms.Domain.Entities.Documents.Adjustment;
+using Nimbo.Wms.Domain.Identification;
+
+namespace Nimbo.Wms.Infrastructure.Persistence.Repositories.Documents;
+
+public class EfAdjustmentDocumentRepository : EfDocumentRepository<AdjustmentDocument, AdjustmentDocumentId>, IAdjustmentDocumentRepository
+{
+    public EfAdjustmentDocumentRepository(NimboWmsDbContext dbContext)
+        : base(dbContext) { }
+
+    public override Task<AdjustmentDocument?> GetByIdAsync(AdjustmentDocumentId id, CancellationToken ct = default)
+    {
+        return Set.Include(d => d.Lines)
+            .FirstOrDefaultAsync(d => d.Id == id, ct);
+    }
+}
