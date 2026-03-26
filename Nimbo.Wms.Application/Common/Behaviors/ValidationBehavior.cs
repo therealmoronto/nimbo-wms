2299c742e7856d468676873f7a0b4e89977d4e58
Alexander Morgunov <toruk010994@gmail.com>
2026-03-26 14:37:58 +0400

    nimbo-wms-155: Pass `CancellationToken` to `next` delegate in `ValidationBehavior`
    - Updated `ValidationBehavior` to include `CancellationToken` when invoking the `next` delegate.
    - Ensured proper task cancellation propagation within the validation pipeline.

--- Nimbo.Wms.Application/Common/Behaviors/ValidationBehavior.cs
+++ Nimbo.Wms.Application/Common/Behaviors/ValidationBehavior.cs
@@ -10,7 +10,7 @@ public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TReq
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
     {
         if (!validators.Any())
-            return await next();
+            return await next(ct);
 
         var context = new ValidationContext<TRequest>(request);
         var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, ct)));
@@ -23,6 +23,6 @@ public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TReq
         if (failures.Count != 0)
             throw new ValidationException(failures);
 
-        return await next();
+        return await next(ct);
     }
 }
