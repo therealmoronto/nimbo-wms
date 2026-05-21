ba84089d199460758c1bcd7279803b5b73f5fd5a
Alexander Morgunov <toruk010994@gmail.com>
2026-05-21 22:24:56 +0400

    nimbo-wms-199: Register shipment and cycle count document mappers in DI container

--- Nimbo.Wms.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Nimbo.Wms.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -10,16 +10,20 @@ using Nimbo.Wms.Application.Abstractions.Persistence.Repositories.MasterData;
 using Nimbo.Wms.Application.Abstractions.Persistence.Repositories.Stock;
 using Nimbo.Wms.Application.Abstractions.Persistence.Repositories.Topology;
 using Nimbo.Wms.Application.Common.Behaviors;
+using Nimbo.Wms.Application.Mappings.Documents.CycleCount;
 using Nimbo.Wms.Application.Mappings.MasterData;
 using Nimbo.Wms.Application.Mappings.Stock;
 using Nimbo.Wms.Application.Mappings.Topology;
 using Nimbo.Wms.Application.Mappings.Documents.Receiving;
+using Nimbo.Wms.Application.Mappings.Documents.Shipment;
 using Nimbo.Wms.Application.Services.Documents;
 using Nimbo.Wms.Contracts.Common;
+using Nimbo.Wms.Contracts.Documents.CycleCount.Dtos;
 using Nimbo.Wms.Contracts.MasterData.Dtos;
 using Nimbo.Wms.Contracts.Stock.Dtos;
 using Nimbo.Wms.Contracts.Topology.Dtos;
 using Nimbo.Wms.Contracts.Documents.Receiving.Dtos;
+using Nimbo.Wms.Contracts.Documents.Shipment.Dtos;
 using Nimbo.Wms.Domain.Entities.Documents.Adjustment;
 using Nimbo.Wms.Domain.Entities.Documents.CycleCount;
 using Nimbo.Wms.Domain.Entities.Documents.Receiving;
@@ -118,6 +122,10 @@ public static class ServiceCollectionExtensions
         {
             services.AddScoped<IMapper<ReceivingDocument, ReceivingDocumentBodyDto>, ReceivingDocumentBodyMapper>();
             services.AddScoped<IMapper<ReceivingDocumentLine, ReceivingDocumentLineDto>, ReceivingDocumentLineMapper>();
+            services.AddScoped<IMapper<ShipmentDocument, ShipmentDocumentBodyDto>, ShipmentDocumentBodyMapper>();
+            services.AddScoped<IMapper<ShipmentDocumentLine, ShipmentDocumentLineDto>, ShipmentDocumentLineMapper>();
+            services.AddScoped<IMapper<CycleCountDocument, CycleCountDocumentBodyDto>, CycleCountDocumentBodyMapper>();
+            services.AddScoped<IMapper<CycleCountDocumentLine, CycleCountDocumentLineDto>, CycleCountDocumentLineMapper>();
 
             services.AddScoped<IDocumentPostingService<ReceivingDocument>, ReceivingDocumentPostingService>();
             services.AddScoped<IDocumentPostingService<RelocationDocument>, RelocationDocumentPostingService>();
