a1094e9560ebd69c76fa92472cab36a1c2ba2747
Alexander Morgunov <toruk010994@gmail.com>
2026-09-07 16:47:49 +0400

    Add ResultExtensions for converting Result objects to IActionResult
    - Implement extension methods `ToActionResult` and `ToActionResult<T>` for mapping `Result` and `Result<T>` to appropriate HTTP responses.
    - Introduce internal `Problem` method for structured error handling based on `ErrorType`.

---
+++ Nimbo.Wms/Extensions/ResultExtensions.cs
@@ -0,0 +1,41 @@
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Nimbo.Wms.Contracts;
+
+namespace Nimbo.Wms.Extensions;
+
+[PublicAPI]
+public static class ResultExtensions
+{
+    public static IActionResult ToActionResult(this Result result, ControllerBase controller)
+    {
+        return result.IsSuccess
+            ? controller.NoContent()
+            : controller.Problem(result.Error);
+    }
+
+    public static IActionResult ToActionResult<T>(this Result<T> result, ControllerBase controller)
+    {
+        return result.IsSuccess
+            ? controller.Ok(result.Value)
+            : controller.Problem(result.Error);
+    }
+
+    private static ObjectResult Problem(this ControllerBase controller, Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.BusinessRule => StatusCodes.Status422UnprocessableEntity,
+            ErrorType.Unexpected => StatusCodes.Status500InternalServerError,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        return controller.Problem(
+            detail: error.Message,
+            title: error.Code,
+            statusCode: statusCode);
+    }
+}
